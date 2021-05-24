c66e004d2b4fcf2ce6c950e17555582b330136ad
U170709040 <fadimedurna@posta.mu.edu.tr>
2021-05-24 17:09:46 +0300

    added some changes to my repo!

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ const Todo = require("../models/Todo");
 
 router.get("/", async(req, res) => {//<------Main GET
     const allTodo = await Todo.find();
-    res.send("Working!!")
     res.render("index", {todo: allTodo})
 })
 
