2965ad63146c5fb60ce69507074d2e3c7b9fe0d5
U170709040 <fadimedurna@posta.mu.edu.tr>
2021-05-25 04:08:15 +0300

    proc, json and mongodb-atlas changes

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const Todo = require("../models/Todo");
 //routes will be here...
 
 router.get("/", async(req, res) => {//<------Main GET
+    
     const allTodo = await Todo.find();
     res.render("index", {todo: allTodo})
 })
